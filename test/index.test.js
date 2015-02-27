82a7a42b4cda7f851dc9d15d2810450f4f19e14d
dak0rn <daniel@suitsoft.eu>
2015-02-27 10:22:53 +0100

    Tests restructured

--- test/index.test.js
+++ test/index.test.js
@@ -12,10 +12,6 @@ describe('Interface', function() {
 		expect( index.length ).to.equal(1);
 	});
 
-});
-
-describe('Plugin', function(){
-
 	it('should expose a function in grunt', function(){
 
 		index(grunt);
